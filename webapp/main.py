43f6931ba6301e7dd2204a917aceb016856ddf3d
Alfredo Deza <alfredo@deza.pe>
2022-08-27 14:31:53 +0000

    working time generate func

--- webapp/main.py
+++ webapp/main.py
@@ -5,7 +5,6 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
-generator = pipeline('text-generation', model='openai-gpt')
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
 
@@ -13,7 +12,7 @@ app = FastAPI()
 app.mount("/ui", StaticFiles(directory=static_path), name="ui")
 
 class Body(BaseModel):
-    text: str
+    strftime: str
 
 
 @app.get('/')
@@ -27,5 +26,5 @@ def generate(body: Body):
     Generate the current time given a strftime template. For example:
     '%Y-%m-%dT%H:%M:%S.%f'
     """
-    tmpl = body.text or '%Y-%m-%dT%H:%M:%S.%f'
-    return {'date': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}
+    tmpl = body.strftime or '%Y-%m-%dT%H:%M:%S.%f'
+    return {'date': datetime.now().strftime(tmpl)}
